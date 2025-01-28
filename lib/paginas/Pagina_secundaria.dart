64713a33cfff27a65a92cf228cbee487f5f51e0f
ethan gil <ethangilfranco@gmail.com>
2025-01-28 16:07:44 +0100

    Todas las clases

---
+++ lib/paginas/Pagina_secundaria.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Peliculas extends StatelessWidget {
+  const Peliculas({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      //Poner comentario y guardarlo
+
+
+      //Fondo
+
+
+      //Plantilla pelicula
+
+      //Videos troll en las pelis(obligatorio/opcional)
+
+
+    );
+  }
+}
---
+++ lib/paginas/pagina_principal.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Netflix extends StatelessWidget {
+  const Netflix({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+
+
+    //Fondo
+
+
+    //Barra navegacion(opcional si hay tiempo)
+
+
+    //Inicio de sesion
+
+
+    //BBDD que esta en el ultimo proyecto
+
+
+    //Peliculas
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+}
